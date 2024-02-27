1058eed25b69f83abbe329f62faf3d1e33fcc6f3
peninah98 <peninangizwenayo@gmail.com>
2024-02-27 12:27:38 +0200

    refactor: added styles on the navbad

--- src/Component/NavBar.tsx
+++ src/Component/NavBar.tsx
@@ -5,18 +5,19 @@ import logo from "/Logo.png"
 export default function NavBar(): ReactElement {
   const items = navItems.map((item) => (
     <div
-      className="text-white hover:text-yellow-400 hover:scale-110 cursor-pointer"
+      className="text-white hover:text-yellow-400 hover:scale-110 cursor-pointer px-4"
       key={item.title}>
       {item.title}
     </div>
   ))
   return (
-    <div className="bg-gray-700 hover:font-semibold sticky top-0 z-50">
+    <div className="bg-gray-700 hover:font-semibold sticky top-0 z-50 py-6 hidden md:flex justify-between items-center">
       <img
         src={logo}
         alt="Penine"
+        className="h-10 ml-6"
       />
-      <div className="flex justify-between py-8">{items}</div>
+      <div className="flex gap-10">{items}</div>
     </div>
   )
 }
