3fd58b8132eb185444777f2268292aab5f8c2643
peninah98 <peninangizwenayo@gmail.com>
2024-02-27 12:21:12 +0200

    feat:responsiveness of the navbat

--- src/Component/NavBar.tsx
+++ src/Component/NavBar.tsx
@@ -5,18 +5,18 @@ import logo from "/Logo.png"
 export default function NavBar(): ReactElement {
   const items = navItems.map((item) => (
     <div
-      className="text-white hover:text-yellow-400 hover:scale-110 cursor-pointer hover:font-semibold"
+      className="text-white hover:text-yellow-400 hover:scale-110 cursor-pointer"
       key={item.title}>
       {item.title}
     </div>
   ))
   return (
-    <div className="bg-gray-700 flex justify-between items-center py-8">
+    <div className="bg-gray-700 hover:font-semibold sticky top-0 z-50">
       <img
         src={logo}
-        alt="Penine."
+        alt="Penine"
       />
-      <div className="flex justify-between gap-20">{items}</div>
+      <div className="flex justify-between py-8">{items}</div>
     </div>
   )
 }
