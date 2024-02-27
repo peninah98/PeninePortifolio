020b01592afdd3421076d032ea14c9333f91a871
peninah98 <peninangizwenayo@gmail.com>
2024-02-27 11:04:05 +0200

    feat: creating footer

--- src/Component/NavBar.tsx
+++ src/Component/NavBar.tsx
@@ -1,18 +1,22 @@
 import { ReactElement } from "react"
 import { navItems } from "../util/navItems"
-
+import logo from "/Logo.png"
 
 export default function NavBar(): ReactElement {
-  const items = navItems.map((item) => <div key={item.title}>{item.title}
-  
-  </div>)
+  const items = navItems.map((item) => (
+    <div
+      className="text-white hover:text-yellow-400 hover:scale-110 cursor-pointer hover:font-semibold"
+      key={item.title}>
+      {item.title}
+    </div>
+  ))
   return (
-    <div className="bg-gray-700">
+    <div className="bg-gray-700 flex justify-between items-center py-8">
       <img
-        src=""
-        alt=""
+        src={logo}
+        alt="Penine."
       />
-      <div>{items}</div>
+      <div className="flex justify-between gap-20">{items}</div>
     </div>
   )
 }
