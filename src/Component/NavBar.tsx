8bad478739b5a7075f0fe5ab8d2b2b49aa6e96c1
peninah98 <peninangizwenayo@gmail.com>
2024-02-27 09:54:52 +0200

    feat: creating new branch for development

--- src/Component/NavBar.tsx
+++ src/Component/NavBar.tsx
@@ -3,9 +3,12 @@ import { ReactElement } from "react"
 export default function NavBar(): ReactElement {
   return (
     <div className="bg-gray-700">
-      <img src="" alt="" />
+      <img
+        src=""
+        alt=""
+      />
       <ul className=" text-white flex justify-between items-center h-16 px-4 mx-10">
-        
+        <li>Home</li>
         <li>About</li>
         <li>Skills</li>
         <li>Project</li>
