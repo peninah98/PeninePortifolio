48de46f0db6dfc62d1f31c0507c7eeb77a27ce7b
peninah98 <peninangizwenayo@gmail.com>
2024-02-27 15:41:57 +0200

    feat: creating responsive navbar:

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,7 @@
-import NavBar from "./Component/NavBar"
 import "./index.css"
 
 function App() {
-  return (
-    <div>
-      <NavBar />
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga commodi
-      voluptatibus quas veniam magni harum praesentium, inventore quibusdam
-      tenetur assumenda earum architecto quisquam officia nisi sunt ut quasi
-      placeat ullam! Ipsa itaque tenetur exercitationem natus iste corrupti ab
-      quos doloremque quam? Nemo est aliquid quis velit aut officiis labore
-      architecto adipisci quo accusantium quam similique doloremque temporibus,
-      molestiae omnis voluptatibus nesciunt, ut ab quod nihil, quia voluptatum.
-      Aperiam adipisci nesciunt consequatur eius quibusdam ea, cumque, quo
-      quidem porro necessitatibus itaque aspernatur illo pariatur. Iure,
-      laudantium quaerat? Laborum, ex nobis! Ad eaque ducimus alias unde
-      consectetur, distinctio eligendi a voluptates pariatur recusandae
-      temporibus, quae doloremque accusantium vel. Cumque totam deleniti fugiat
-      magnam ipsa ratione quisquam sunt iure repellendus tempora soluta adipisci
-      reiciendis necessitatibus optio sint, neque voluptas id repudiandae
-      nesciunt magni. Quasi asperiores quibusdam ea! Non nihil delectus eaque ex
-      tempore aperiam vero eveniet alias ullam! Perspiciatis, sint, ratione
-      dignissimos error, ullam earum magni officia aspernatur nam repellat odio
-      quidem cumque hic commodi alias et! Maxime commodi sequi debitis? Repellat
-      placeat blanditiis voluptatum magnam, fugit vero nesciunt tempore,
-      reiciendis incidunt libero facilis enim nulla architecto ipsam
-      exercitationem excepturi in alias, harum velit sequi quod distinctio
-      corporis. Ipsum officia pariatur aspernatur suscipit voluptate vero
-      aperiam amet rem culpa neque voluptatibus dolorum alias debitis recusandae
-      quod animi accusantium quia quas, sed a optio corrupti repellendus quis
-      minima! Delectus, cum aliquid error facere similique eius illo non
-      veritatis veniam et quo eveniet quidem deleniti asperiores, doloremque
-      quos eum sequi. Adipisci consequuntur repellat, ad quas cupiditate illum
-      veritatis eius voluptatibus, perferendis quae ex laborum? Veniam, sed?
-      Similique, nihil? Earum incidunt nobis optio unde nisi repudiandae,
-      tempore soluta possimus quod repellat. Iste repudiandae, dolorem in cumque
-      tempora labore dolor animi id ab omnis, deleniti quae repellendus. Alias
-      eos atque repellat, harum et corrupti ullam iure delectus aspernatur
-      debitis facere. Quis, deleniti id. Repellendus aperiam, officiis vero
-      doloribus magni sed exercitationem dignissimos cupiditate quia delectus
-      cum laudantium repellat vitae magnam ipsam voluptatibus voluptates
-      asperiores, dolores non placeat nulla nemo! Est officiis sequi,
-      perspiciatis, alias molestiae optio facilis, ratione atque omnis provident
-      voluptatum. Cum ab voluptatem eum optio quod aut distinctio quasi commodi,
-      laboriosam molestias molestiae architecto! Quia asperiores unde commodi
-      animi consequatur et ex perferendis? Accusamus sint ducimus non! Minus ex
-      voluptate ad earum soluta nulla sequi id delectus qui architecto dolorem
-      est magnam itaque doloribus cumque aperiam incidunt et aut, voluptates
-      inventore! Obcaecati nemo explicabo necessitatibus distinctio velit,
-      corrupti ullam! Neque quisquam quae asperiores? Optio aut vel quod ipsa
-      nihil itaque nemo nisi repellat, assumenda velit at sit excepturi! Eaque,
-      numquam? Soluta, cum reiciendis nesciunt voluptates tenetur explicabo
-      consequatur nobis repellendus recusandae laboriosam vel, velit iusto.
-      Dicta laborum quaerat velit, quis ipsum nobis tempore quam ducimus quasi
-      pariatur eaque dolores ratione, nisi repellendus beatae minima.
-      Consequatur dolores architecto obcaecati saepe aliquam, quisquam quia
-      blanditiis eaque distinctio nemo animi itaque quo dolorum! At dignissimos
-      enim voluptatum architecto dicta totam consectetur amet natus
-      necessitatibus quos, quidem nemo aliquid ea, iste maxime quo sint sunt
-      nisi perspiciatis quae possimus.
-    </div>
-  )
+  return <div></div>
 }
 
 export default App
