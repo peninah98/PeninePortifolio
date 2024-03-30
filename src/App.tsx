38e8649ba17f774b65e516d0b5c716b7c8ac4c7d
peninah98 <peninangizwenayo@gmail.com>
2024-03-30 13:34:53 +0200

    feat: navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import NavBar from "./Component/NavBar"
 import "./index.css"
 
 function App() {
-  return <div></div>
+  return (
+    <div>
+      <NavBar />
+    </div>
+  )
 }
 
 export default App
