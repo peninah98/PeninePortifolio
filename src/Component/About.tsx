7bca823522e92b1e710996d1ad29dbdbc6b40797
peninah98 <peninangizwenayo@gmail.com>
2024-03-30 13:50:04 +0200

    feat: created about page

--- src/Component/About.tsx
+++ src/Component/About.tsx
@@ -1,5 +1,24 @@
 function About() {
-  return <div>About page</div>
+  return (
+    <div className="bg-[#CCCCCC] rounded-l-2xl grid md:grid-cols-2 gap-6 ">
+      <div>
+        <h1 className="text-gray-500 text-xl">About</h1>
+        <div className="font-normal text-lg p-10">
+          I am software developer who always eager to learn new things. I am
+          looking forward to use my skills and expertise to contribute o
+          projects that has good impact on users.
+        </div>
+      </div>
+      <div>
+        <h1 className="text-gray-500 text-xl">Experience</h1>
+        <div className="font-normal text-lg p-10">
+          I am software developer who always eager to learn new things. I am
+          looking forward to use my skills and expertise to contribute o
+          projects that has good impact on users.
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default About
