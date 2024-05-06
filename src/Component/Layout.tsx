8f391135f6e2c8022f8c6a626415c2b01c69fd97
Penine  Ngizwenayo <peninangizwenayo@gmail.com>
2024-05-06 19:25:08 +0200

    refactor: deleting some imges

--- src/Component/Layout.tsx
+++ src/Component/Layout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom"
 import NavBar from "./NavBar"
+import Counter from "./Counter"
 
 export default function Layout() {
   return (
     <>
       <NavBar />
+      <Counter />
       <Outlet />
     </>
   )
